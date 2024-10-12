a03fd4f842387a7847ba5e2fee690fe7c8ae223d
Seinzzz <huseinzidane29@gmail.com>
2024-10-13 01:56:52 +0700

    refactor: :heavy_plus_sign: add plugin prettier-tailwindcss and prettier config

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
+  <div class="flex justify-center">
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
