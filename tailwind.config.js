729fa1458b1b95679c33a0bf409db433da46c51d
ramaboy13 <baritosurya13@gmail.com>
2024-10-17 22:26:31 +0700

    Nothing!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
+tailwind.config.js
