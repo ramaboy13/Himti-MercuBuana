1461da7e6a3041c28e8336c15be8091758b34246
Seinzzz <huseinzidane29@gmail.com>
2024-10-25 01:30:06 +0700

    feat: :sparkles: tambahin waves
    Update package.json to version 0.0.3 and tailwind.config.js to include a dark mode option.  Update index.html and NavbarSection.vue  Update the background color classes in index.html and NavbarSection.vue to match the tailwind.config.js colors.  Add new files  Add gradient.svg to src/assets/img/ and wave_down.svg to src/assets/waves/.  Refactor code  Refactor NavbarSection.vue to improve readability and maintainability.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
     './node_modules/preline/preline.js',
   ],
-  darkMode: 'class',
   theme: {
     extend: {
       colors: {
+        primary: '#180D2A',
+        secondary: '#7E3874',
+        accent: '#7B4399',
         'main-1': '#180D2A',
         'text-main-1': '#7B4399',
         'accent-7': '#333',
