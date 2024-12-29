66d2afd2fa83e3a8536617d4e3e565afde23e887
Barito Surya Ramadhani <42413899+ramaboy13@users.noreply.github.com>
2024-12-29 14:04:21 +0700

    fixed : image member to avif format

--- src/pages/MemberPages.vue
+++ src/pages/MemberPages.vue
@@ -60,31 +60,6 @@ export default {
       </p>
 
       <div class="hs-dropdown relative inline-flex">
-        <button
-          id="hs-dropdown-default"
-          type="button"
-          class="hs-dropdown-toggle inline-flex items-center gap-x-2 rounded-lg border border-gray-200 bg-white px-4 py-3 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 focus:bg-gray-50 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-800 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-          aria-haspopup="menu"
-          aria-expanded="false"
-          aria-label="Dropdown"
-        >
-          Actions
-          <svg
-            class="size-4 hs-dropdown-open:rotate-180"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="m6 9 6 6 6-6" />
-          </svg>
-        </button>
-
         <div
           class="hs-dropdown-menu duration mt-2 hidden min-w-60 rounded-lg bg-white opacity-0 shadow-md transition-[opacity,margin] before:absolute before:-top-4 before:start-0 before:h-4 before:w-full after:absolute after:-bottom-4 after:start-0 after:h-4 after:w-full hs-dropdown-open:opacity-100 dark:divide-neutral-700 dark:border dark:border-neutral-700 dark:bg-neutral-800"
           role="menu"
